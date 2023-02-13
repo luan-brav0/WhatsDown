680543ed1e7900093f062b3957de712c8d781bb9
luan-brav0 <luan.melo.bravo10@gmail.com>
2023-02-12 23:19:10 -0300

    message converted from string to object

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,36 +1,52 @@
-import React, { useState } from 'react'
+import React, { FC, useState } from 'react'
 import { StyleSheet, TextInput, Text, View, Button, FlatList } from 'react-native'
 
 type Props = {}
 
-const Chat = (props: Props) => {
-    const [inputValue, setInputValue] = useState<string>('');
-    const [messages, setMessages] = useState<string[]>([]);
-    const handleButtonPress = () => {
-        if (inputValue) {
-            setMessages(prevMessages => [...prevMessages, inputValue])
-            setInputValue("")
-            console.log(messages)
-        }
+const Chat: FC<Props> = (props): JSX.Element => {
+    type Message = {
+        key: string,
+        content: string,
+        chat?: string,  
+        time?: Date,
+        author?: string,
+        authorPhotoPath?: string,
     };
+    const [inputValue, setInputValue] = useState<string>('');
+    const [messages, setMessages] = useState<Message[]>([]);
 
+    const _sendPress = (): void => {
+        if (inputValue) {
+            let curAuthor = 'lul'
+            let curTime = new Date();
+
+            let curMessage: Message = { key: `${inputValue} @ ${curTime.toString()}`, content: inputValue, time: curTime, author: curAuthor  };
+
+            setMessages(prevMessages => [...prevMessages, curMessage]);
+
+            setInputValue("");
+        };
+    }
+
+    const _onChangeText = (text: string): void => setInputValue(text)
     return (
         <View style={styles.container}>
-            {messages ?
-                <FlatList style={styles.messagesCont}
-                    data={messages}
-                    renderItem={({ item }: { item: string }) => <Text style={styles.message}>{item}</Text>}
-                />
-                : null}
-
+            {messages &&
+                (<View style={styles.messagesCont}>
+                    {messages.map(({key, content, author, time }) => (
+                        <Text key={key} style={styles.message}> {content} by {author} @ {time?.toString()}</Text>
+                    ))}
+                </View>)
+            }
             <View style={styles.inputCont}>
                 <TextInput style={styles.input}
                     placeholder="Message"
-                    onChangeText={(text: string) => setInputValue(text)}
+                    onChangeText={_onChangeText}
                     value={inputValue} />
+
                 <Button color='blueviolet'
                     title={'send'}
-                    onPress={handleButtonPress} />
+                    onPress={_sendPress} />
             </View>
         </View>
     )
@@ -53,6 +69,15 @@ const styles = StyleSheet.create({
     },
     message: {
         backgroundColor: 'cyan'
+    },
+    messageCont: {
+
+    },
+    messageTime: {
+
+    },
+    ownerCont: {
+
     },
     inputCont: {
         backgroundColor: 'red',
